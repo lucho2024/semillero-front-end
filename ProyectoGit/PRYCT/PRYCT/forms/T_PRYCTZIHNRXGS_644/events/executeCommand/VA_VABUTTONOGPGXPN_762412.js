94fc9fcbf4c7b70ffb84a910a7f83e15e4242480
ginkathy <gingersc@hotmail.com>
2021-04-16 12:03:42 -0500

    Cambios GSC

---
+++ ProyectoGit/PRYCT/PRYCT/forms/T_PRYCTZIHNRXGS_644/events/executeCommand/VA_VABUTTONOGPGXPN_762412.js
@@ -0,0 +1,11 @@
+
+
+//Entity: PersonaGSC
+//PersonaGSC. (Button) View: EstudianteGscForm
+//Evento ExecuteCommand: Permite personalizar la acción a ejecutar de un command o de un ActionControl.
+task.executeCommand.VA_VABUTTONOGPGXPN_762412 = function(  entities, executeCommandEventArgs ) {
+
+    executeCommandEventArgs.commons.execServer = true;
+    //executeCommandEventArgs.commons.serverParameters.PersonaGSC = true;
+
+};
